d90246742bb766c1cb240f6faf826e3dc5c393a8
Sagar <sagarbhanushali0120@gmail.com>
2022-06-07 00:47:52 +0530

    create basic structure of Register page

---
+++ src/components/Register.js
@@ -0,0 +1,13 @@
+function Register(){
+    return(
+        <div>
+            <h1>Register Page</h1>
+            <input type="text" placeholder="Name"/><br />
+            <input type="password" placeholder="Password" /><br />
+            <input type="email" placeholder="Email Id"/><br />
+            <button>Sing Up</button>
+        </div>
+    )
+}
+
+export default Register
