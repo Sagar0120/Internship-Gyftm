38d913b1e0bf411869e78757189c79a9c0f1ebc2
Sagar <sagarbhanushali0120@gmail.com>
2022-06-11 18:45:09 +0530

    verify OTP done

--- src/Service/DataService.js
+++ src/Service/DataService.js
@@ -4,7 +4,6 @@ const Service_Url='https://api.shilpimultiplex.com/api'
 
 ///Auth/CreateUser
 
-
 class DataService {
   createUser(user) {
     return axios.post(Service_Url + "/Auth/CreateUser", user);
@@ -15,6 +14,10 @@ class DataService {
   LoginUser(user){
     return axios.post(Service_Url + "/Auth/Authenticate", user);
   }
+  VerifyOTP(user){
+    return axios.post(Service_Url + "/Auth/VerifyOtp?uid="+user);
+  }
+  //a81c54b9-b10d-428f-b721-9398a97af022&otp=123456
 }
 
 export default new DataService();
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,130 +1,157 @@
-import React from 'react';
-import DataService from '../Service/DataService';
+import React from "react";
+import DataService from "../Service/DataService";
+import axios from "axios";
+
+const API_URL = "https://api.shilpimultiplex.com/api/Auth/";
+
+//const [otp, setOtp] = useState('');
+
+let Uid;
 
 class Register extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-        name : "",
-        email: "",
-        phone: "",
-        password: ""
+      name: "",
+      email: "",
+      phone: "",
+      password: "",
+      otp: "",
     };
     this.changenamehandler = this.changenamehandler.bind(this);
     this.changemailhandler = this.changemailhandler.bind(this);
     this.changephonehandler = this.changephonehandler.bind(this);
     this.changepasshandler = this.changepasshandler.bind(this);
-}
+    this.changOTPhandler = this.changOTPhandler.bind(this);
+  }
 
-    saveUser = (e) => {
-        if(e && e.preventDefault) {
-            e.preventDefault();
-            e.persist();
+  saveUser = (e) => {
+    if (e && e.preventDefault) {
+      e.preventDefault();
+      e.persist();
+    }
+
+    let user = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      password: this.state.password,
+    };
+    console.log("user => " + JSON.stringify(user));
+
+    DataService.createUser(user).then((Response) => {
+      console.log("user id : ", Response.data.id);
+      Uid = Response.data.id;
+
+      DataService.sendOTP(Response.data.id).then((Resp) => {
+        console.log(Resp);
+      });
+    });
+
+    // "a81c54b9-b10d-428f-b721-9398a97af022"
+    //let Uid = {uid : Object.uid}
+  };
+
+  VeryfiyUser = (e) => {
+    if (e && e.preventDefault) {
+      e.preventDefault();
+      e.persist();
+    }
+    axios
+      .post(API_URL + "VerifyOtp?uid=" + Uid + "&otp=" + this.state.otp)
+      .then((result) => {
+        if (result.request.status === 200) {
+          console.log(result);
         }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-        let user = {name: this.state.name, email: this.state.email, phone:this.state.phone, password:this.state.password};
-        console.log('user => '+JSON.stringify(user));
+  changenamehandler = (event) => {
+    this.setState({ name: event.target.value });
+  };
 
-        DataService.createUser(user).then(Response => {
-            console.log("user id : ", Response.data.id);
-            
-            DataService.sendOTP(Response.data.id).then(Resp => {
-                console.log(Resp);
-            })
-        })
+  changemailhandler = (event) => {
+    this.setState({ email: event.target.value });
+  };
 
-        // "a81c54b9-b10d-428f-b721-9398a97af022"
-        //let Uid = {uid : Object.uid}
-        
-    }
+  changephonehandler = (event) => {
+    this.setState({ phone: event.target.value });
+  };
 
-    changenamehandler = (event) => {
-        this.setState({name: event.target.value})
-    }
+  changepasshandler = (event) => {
+    this.setState({ password: event.target.value });
+  };
 
-    changemailhandler = (event) => {
-        this.setState({email: event.target.value})
-    }
+  changOTPhandler = (event) => {
+    this.setState({ otp: event.target.value });
+  };
 
-    changephonehandler = (event) => {
-        this.setState({phone: event.target.value})
-    }
+  render() {
+    return (
+      <>
+        <section>
+          <form>
+            <label>Name : </label>
+            <input
+              placeholder="Name"
+              name="name"
+              className="form-control"
+              value={this.state.name}
+              onChange={this.changenamehandler}
+            />
 
-    changepasshandler = (event) => {
-        this.setState({password: event.target.value})
-    }
+            <label>Email : </label>
+            <input
+              placeholder="Email"
+              name="email"
+              className="form-control"
+              value={this.state.email}
+              onChange={this.changemailhandler}
+            />
 
-    render() {
-        return(
-            // <div>
-            //     <h1>Register Page</h1>
-            //     <input type="text" value={this.state.name} name="name" onChange = {this.changenamehandler} placeholder="Name"/><br />
-            //     <input type="email" value={this.state.email} name="email" onChange = {this.changemailhandler} placeholder="Email Id"/><br />
-            //     <input type="number" value={this.state.phone} name="phone" onChange = {this.changephonehandler} placeholder="Mobile Number"/><br />
-            //     <input type="password" value={this.state.password} name="password" onChange = {this.changepasshandler} placeholder="Password" /><br />
-            //     <button onClick={this.saveUser}>Sing Up</button>
-            // </div>
+            <label>Phone : </label>
+            <input
+              placeholder="phone"
+              name="phone"
+              className="form-control"
+              value={this.state.phone}
+              onChange={this.changephonehandler}
+            />
 
-            <div>
-                <section>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='card-body'>
-                            <form>
-                                <div className='form-group'>
-                                    <label>Name : </label>
-                                    <input
-                                    placeholder='Name'
-                                    name="name"
-                                    className='form-control'
-                                    value={this.state.name}
-                                    onChange={this.changenamehandler} />
-                                </div>
-                                <p> </p>
-                                <div className='form-group'>
-                                    <label>Email : </label>
-                                    <input
-                                    placeholder='Email'
-                                    name="email"
-                                    className='form-control'
-                                    value={this.state.email}
-                                    onChange={this.changemailhandler} />
-                                </div>
-                                <p> </p>
-                                <div className='form-group'>
-                                    <label>Phone : </label>
-                                    <input
-                                    placeholder='phone'
-                                    name="phone"
-                                    className='form-control'
-                                    value={this.state.phone}
-                                    onChange={this.changephonehandler} />
-                                </div>
-                                <p> </p>
-                                <div className='form-group'>
-                                    <label>Password : </label>
-                                    <input type = "password"
-                                    placeholder='password'
-                                    name="password"
-                                    className='form-control'
-                                    value={this.state.password}
-                                    onChange={this.changepasshandler} />
-                                </div>
-                                <p> </p>
-                                <div className="text-center">
-                                    <button className="btn btn-success" onClick={this.saveUser}>Create user</button>
+            <label>Password : </label>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.changepasshandler}
+            />
 
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                </section>
-            </div>
-        )
-    }
+            <button className="btn btn-success" onClick={this.saveUser}>
+              Create user
+            </button>
+          </form>
+        </section>
+        <form>
+          {/*modal for otp*/}
+          <h5>Enter your OTP</h5>
+          <input
+            type="text"
+            value={this.state.otp}
+            onChange={this.changOTPhandler}
+          />
+          <button type="button" onClick={this.VeryfiyUser}>
+            Ok
+          </button>
+        </form>
+      </>
+    );
+  }
 }
-
 
 export default Register;
